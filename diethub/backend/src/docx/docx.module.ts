90575fa2ad0fb17e4ff8606f3603fd3eb2626756
Davi Mateus Silveira <david2fagulha@gmail.com>
2025-04-07 18:24:20 -0300

    Backend removal from diethub directory

--- diethub/backend/src/docx/docx.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DocxService } from './docx.service';
-
-@Module({
-  providers: [DocxService],
-  exports: [DocxService],
-})
-export class DocxModule {}
